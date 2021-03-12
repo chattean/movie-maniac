05c2182de44c35b602e0b2973cb9f59efb1db00b
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-03-12 11:22:41 -0800

    saving so i canpull from origin

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
 // set up Mongoose to connect when we start the app
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/movie-maniac', {

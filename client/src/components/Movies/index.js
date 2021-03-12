f5d0032b74b4d2c111614c87e090897d9b3a9eeb
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-03-11 19:52:45 -0800

    saving to test

--- client/src/components/Movies/index.js
+++ client/src/components/Movies/index.js
@@ -1,30 +1,47 @@
 import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import MovieList from '../MovieList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-// import { ADD_TO_WATCHLIST } from '../../utils/actions';
+import { ADD_TO_WATCHLIST } from '../../utils/actions';
 
-// const addToWatchList = () => {
-//   const movieOnList = movieList.find((movieListItem) => movieListItem._id === _id)
-//   if (movieOnList) {
-//     // tell user the movie is already on the list
-//   } else {
-//     // not on list put it on the list
-// dispatch({
-//   type: ADD_TO_WATCHLIST,
-//   movie: { ...movie }
-// });
-//   }
-// }
 
-function Movies(props) {
-  const { currentCategory } = props;
+
+function Movies(movie) {
+
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+
+  const {
+    image,
+    name,
+    _id,
+
+  } = movie;
+
+  const { watchList } = state
+
+  const addToWatchList = () => {
+    const movieOnList = MovieList.find((movieListItem) => movieListItem._id === _id)
+    if (movieOnList) {
+      // tell user the movie is already on the list
+    } else {
+      // not on list put it on the list
+      // dispatch({
+      //   type: ADD_TO_WATCHLIST,
+      //   movie: { ...movie }
+      // });
+    }
+  }
+
+
+  const { currentCategory } = movie;
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.description}</p>
       <MovieList category={currentCategory.name} />
-      <button>Add to My Watch List</button>
-      {/* // onClick={addToWatchList} add in button> */}
+      <button onClick={addToWatchList}>Add to Watch List</button>
+
       <button><span role="img" aria-label="heart">💚</span></button>
       {/* add a counter to the heart  */}
     </section>
